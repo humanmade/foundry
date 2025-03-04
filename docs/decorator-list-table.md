865419b91a5e2d730e01de27c5989ffea448b8cf
Ryan McCue <me@ryanmccue.info>
2025-03-04 17:56:04 +0000

    Add actions to list table

--- docs/decorator-list-table.md
+++ docs/decorator-list-table.md
@@ -10,3 +10,123 @@ The available column options are:
 * `description`
 * `foundry:render` - Callable, defaults to `Model->get_field( $id )`
 * `foundry:sortable` - Boolean, defaults to true if the data type is sortable.
+
+You'll also need to provide `prepare_model_for_output()`, which should return an associative array mapping from column ID to output.
+
+
+## Column rendering
+
+Each registered column will directly output the data returned from the `prepare_model_for_output()` method.
+
+For more advanced rendering, column rendering can be overridden through two different mechanisms:
+
+* **Schema** - In your schema, you can declare a `foundry:render` callback, which will be passed the data from `prepare_model_for_output()`.
+* **Methods** - You can also use the traditional WordPress-style for list tables, and declare a method which matches the column name, prefixed with `column_`. For example, an `id` column would use a `column_id()` method if declared.
+
+Each type of callback receives the array prepared from `prepare_model_for_output()`. The underlying model is available as the `_model` key if needed.
+
+For example:
+
+```php
+// Callback style.
+protected function get_item_schema() : array {
+	return [
+		'name' => [
+			'title' => 'Name',
+			'foundry:render' => function ( $data ) {
+				// Use the data directly (and don't forget to escape!)
+				return esc_html( 'The name of the model is:' . $data['name'] );
+			}
+		],
+	];
+}
+
+// Method style.
+protected function column_name( $data ) {
+	// Grab the model.
+	/** @var MyModel */
+	$model = $data['_model'];
+
+	// Do something more complex.
+	$name = lookup_name_for_model( $model );
+
+	return esc_html( $name );
+}
+```
+
+
+## Basic Actions
+
+Actions can be added through the standard list table APIs, including implementing `handle_row_actions()`.
+
+However, Foundry provides a higher-level abstraction with a `prepare_actions_for_output()` method. This method is called once per row and receives the model.
+
+By default, the item's internal ID is added as the first action to provide users with context on items; this can be retained by calling `parent::prepare_actions_for_output( $model )`, or overridden by removing it from the array.
+
+```php
+protected function prepare_actions_for_output( $model ) {
+	$actions = parent::prepare_actions_for_output( $model );
+
+	$actions['view'] = sprintf(
+		'<a href="%s">View item</a>',
+		$model->get_permalink(),
+	);
+
+	return $actions;
+}
+```
+
+
+## Advanced Actions
+
+Foundry also includes a powerful helper trait, `Foundry\Admin\WithActions`, which can be used to easily add single and bulk actions to any list table. This trait provides the power to easily register actions and handle user interaction with them without needing to write all the glue code yourself.
+
+To implement this trait, `use` it, and implement the abstract `get_actions()` method:
+
+```php
+use Foundry\Admin\WithActions;
+
+protected function get_actions() : array {
+	return [
+		// Specify a unique action ID as the key.
+		'delete' => [
+			// You can pass both a callback and a permission callback.
+			'callback' => [ $this, 'delete_item' ],
+
+			// The permission callback is used to determine if the action is
+			// available for a specific item.
+			// (For bulk actions, $item will be passed as null.)
+			'permission_callback' => function ( ?Model $item ) {
+				return current_user_can( 'manage_options' );
+			},
+
+			// The action labels can be set statically, or specified as a
+			// callback. The callback receives the item as its only argument.
+			//
+			// (Label is expected to be short and is used inline, while
+			// full_label should provide more context for places like
+			// accessibility text.)
+			'label' => 'Delete',
+			'full_label' => function ( Model $item ){
+				return sprintf( __( 'Delete item "%s"' ), $item->get_title() );
+			},
+
+			// For actions which support bulk processing, you can specify the
+			// multiple flag.
+			'multiple' => true,
+			'multiple_label' => 'Delete items',
+
+			// By default, multiple items will be processed by calling the
+			// callback for each item. For efficiency, you can specify a
+			// bulk callback to process all items at once.
+			'multiple_callback' => [ $this, 'delete_items' ],
+		],
+	];
+}
+```
+
+### Checkbox column
+
+If bulk actions are registed, a special `cb` column will be inserted as the first column with default handling.
+
+A special `foundry:accessibility-text` option may be explicitly specified for this column, which will provide the screen reader text for the label associated with this item (for example, "Select My Post").
--- inc/admin/class-list-table.php
+++ inc/admin/class-list-table.php
@@ -2,9 +2,21 @@
 
 namespace Foundry\Admin;
 
+use Foundry\Database\Model;
+use Foundry\Database\Query;
 use WP_List_Table;
 
 abstract class List_Table extends WP_List_Table {
+	protected Query $query;
+
+	/**
+	 * Queried items.
+	 *
+	 * @internal Override type.
+	 * @var \Foundry\Database\QueryResults
+	 */
+	public $items;
+
 	/**
 	 * Get the model for the command.
 	 *
@@ -22,10 +34,35 @@ abstract class List_Table extends WP_List_Table {
 	/**
 	 * Prepare the model for output.
 	 *
+	 * Data returned from this method is output directly to the browser, and so
+	 * must be escaped by your method (using esc_html() or similar).
+	 *
 	 * @param Model $model Model object.
 	 * @return array|WP_Error Data array on success, or WP_Error object on failure.
 	 */
 	abstract protected function prepare_model_for_output( $model );
+
+	/**
+	 * Prepare actions for output.
+	 *
+	 * This method should return an array of actions that can be taken on each
+	 * model. The key should be a unique ID, and the value should be the HTML
+	 * (typically a link) for the action.
+	 *
+	 * By default, the item's internal ID is displayed.
+	 *
+	 * @param Model $model Model object.
+	 */
+	protected function prepare_actions_for_output( $model ) {
+		$actions = [];
+
+		$actions['id'] = sprintf(
+			'Item #%d',
+			$model->get_id(),
+		);
+
+		return $actions;
+	}
 
 	/**
 	 * Prepares the list of items for displaying.
@@ -37,7 +74,17 @@ abstract class List_Table extends WP_List_Table {
 	 */
 	public function prepare_items() {
 		$model = static::get_model();
-		$this->items = $model::query( [] );
+		$per_page = $this->get_items_per_page( 'edit_' . $model . '_per_page' );
+		$this->query = $model::query( [], [
+			'page' => $this->get_pagenum(),
+			'per_page' => $per_page,
+		] );
+		$this->items = $this->query->get_results();
+
+		$this->set_pagination_args( [
+			'total_items' => $this->items->get_total_available(),
+			'per_page' => $per_page,
+		] );
 	}
 
 	/**
@@ -48,9 +95,23 @@ abstract class List_Table extends WP_List_Table {
 	public function get_columns() {
 		$schema = static::get_item_schema();
 
-		return array_map( function ( $column, $id ) {
-			return $column['title'] ?? $id;
-		}, $schema, array_keys( $schema ) );
+		$columns = [];
+
+		// Register the checkbox column if needed for bulk actions.
+		$bulk_actions = $this->get_bulk_actions();
+		if ( ! empty( $bulk_actions ) || isset( $schema['cb'] ) ) {
+			$columns['cb'] = '<input type="checkbox" />';
+		}
+
+		foreach ( $schema as $id => $column ) {
+			// Skip checkbox, as it's handled earlier.
+			if ( $id === 'cb' ) {
+				continue;
+			}
+
+			$columns[ $id ] = $column['title'] ?? $id;
+		}
+		return $columns;
 	}
 
 	/**
@@ -70,7 +131,14 @@ abstract class List_Table extends WP_List_Table {
 	}
 
 	/**
-	 * @param \Foundry\Database\Model $item
+	 * Render an arbitrary column.
+	 *
+	 * Data is pre-prepared by prepare_model_for_output(), so this simply
+	 * outputs the result of that directly.
+	 *
+	 * @param array $item Data prepared by prepare_model_for_output(). (Includes model under _model key.)
+	 * @param string $column Column ID.
+	 * @return string Raw HTML to be output to the browser.
 	 */
 	public function column_default( $item, $column ) {
 		$schema = static::get_item_schema();
@@ -84,5 +152,63 @@ abstract class List_Table extends WP_List_Table {
 		}
 
 		return $item[ $column ];
+	}
+
+	/**
+	 * Handles the checkbox column output.
+	 *
+	 * @since 4.3.0
+	 * @since 5.9.0 Renamed `$post` to `$item` to match parent class for PHP 8 named parameter support.
+	 *
+	 * @param array $item Data prepared by prepare_model_for_output(). (Includes model under _model key.)
+	 */
+	public function column_cb( $item ) {
+		$schema = static::get_item_schema();
+		/** @var \Foundry\Database\Model */
+		$model = $item['_model'];
+		?>
+		<label class="screen-reader-text" for="cb-select-<?php echo esc_attr( $model->get_id() ) ?>">
+			<?php
+			if ( isset( $schema['cb']['foundry:accessibility-text'] ) ) {
+				/* translators: %s: Post title. */
+				echo esc_html(
+					call_user_func( $schema['cb']['foundry:accessibility-text'], $item )
+				);
+			} else {
+				echo esc_html(
+					'Select item #%d',
+					$model->get_id()
+				);
+			}
+			?>
+		</label>
+		<input
+			id="cb-select-<?php echo esc_attr( $model->get_id() ) ?>"
+			type="checkbox"
+			name="item[]"
+			value="<?php echo esc_attr( $model->get_id() ) ?>"
+		/>
+		<?php
+	}
+
+	/**
+	 * Renders row actions in the primary column.
+	 *
+	 * @param array $item Data prepared by prepare_model_for_output(). (Includes model under _model key.)
+	 * @param string $column_name Current column name.
+	 * @param string $primary Primary column name.
+	 * @return string Actions output for the column.
+	 */
+	protected function handle_row_actions( $item, $column_name, $primary ) {
+		if ( $primary !== $column_name ) {
+			return '';
+		}
+
+		$actions = $this->prepare_actions_for_output( $item['_model'] );
+		if ( empty( $actions ) ) {
+			return '';
+		}
+
+		return $this->row_actions( $actions );
 	}
 }
